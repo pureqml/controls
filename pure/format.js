377fd5c069d049b9d53c13018e4dec130456fe61
Vadim Kravchenko <vadim.kravchenko@gmail.com>
2017-08-29 01:14:07 +0300

    text format helper added

--- pure/format.js
+++ pure/format.js
@@ -1,4 +1,14 @@
 exports.currency = function(v, n, x) {
-  var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
-  return v.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+	var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+	return v.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+}
+
+exports.format = function() {
+	var args = [].slice.call(arguments);
+	var initial = args.shift();
+
+	function replacer (text, replacement) {
+		return text.replace('%s', replacement);
+	}
+	return args.reduce(replacer, initial);
 }
